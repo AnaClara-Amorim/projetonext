bd323a8e079035d4c1ca6556c0a237767bfe39e3
Ana Clara <127421985+AnaClara-Amorim@users.noreply.github.com>
2025-05-13 17:36:20 -0300

    Tentando colocar dark mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,21 +4,23 @@ import {AiFillHome} from 'react-icons/ai';
 import {BsFillInfoCircleFill} from 'react-icons/bs';
 import { IoMdLogIn } from "react-icons/io";
 import Link from 'next/link';
+import DarkModeSwitch from './DarkModeSwitch'; /* Importando botãozinho */
+
 
 export default function Header() {
   return (
     <div className='flex justify-between items-center p-3 max-w-6xl mx-auto'>
-    
-    <Link href={"/"} className='flex gap-1 items-center'>
-      <span className='text-2xl font-bold bg-fuchsia-400 py-1 px-1 rounded-lg'>ClaraFlix</span>
-      <span className='text-xl hidden sm:inline'>Clone</span>
-    </Link>
-    
-    <div className='flex gap-4'> 
-      <MenuItem title="home" address="/" Icon={AiFillHome} />         
-      <MenuItem title="about" address="/about" Icon={BsFillInfoCircleFill} />
-      <MenuItem title="login" address="/login" Icon={IoMdLogIn} />  
-    </div>    
+                   
+      <Link href={"/"} className='flex gap-1 items-center'>
+        <span className='text-2xl text-amber-200 font-bold bg-fuchsia-700 py-1 px-1 rounded-lg'>Claraflix</span>
+      </Link>   
+            
+      <div className='flex items-center gap-4'> 
+        <MenuItem title="home" address="/" Icon={AiFillHome} />         
+        <MenuItem title="about" address="/about" Icon={BsFillInfoCircleFill} />
+        <MenuItem title="login" address="/login" Icon={IoMdLogIn} />
+        <DarkModeSwitch />  
+      </div>    
 
     </div>
   )
--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function MenuItem( {title, address, Icon} ) {
   return (
-    <Link href={address} className='hover: text-fuchsia-700'>
+    <Link href={address} className='hover: text-fuchsia-800'>
       <Icon className= 'text-2xl sm:hidden'/>
       <p className='uppercase hidden sm:inline text-sm'>{title}</p>
     </Link>
