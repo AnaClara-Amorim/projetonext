8b8d35e03cf4ab297ad4169e937f0ea39e5626bf
Ana Clara <127421985+AnaClara-Amorim@users.noreply.github.com>
2025-05-13 18:08:36 -0300

    About page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-5 text-justify space-y-4 indent-3'>
+
+        <h1 className='text-2xl font-medium text-fuchsia-800 '>About</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, optio neque? Nihil sunt molestias hic magnam sequi, reprehenderit quis maxime impedit, eos fugiat dolor commodi repellat. Atque voluptates rem dignissimos.
+        Impedit cum qui quas praesentium eius earum magni mollitia harum perferendis nobis voluptatem quasi quae voluptas beatae tenetur optio suscipit iure, ex atque dolor, facere eum. Reprehenderit, nemo vero. Sint.</p>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, optio neque? Nihil sunt molestias hic magnam sequi, reprehenderit quis maxime impedit, eos fugiat dolor commodi repellat. Atque voluptates rem dignissimos.
+        Impedit cum qui quas praesentium eius earum magni mollitia harum perferendis nobis voluptatem quasi quae voluptas beatae tenetur optio suscipit iure, ex atque dolor, facere eum. Reprehenderit, nemo vero. Sint.</p>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, optio neque? Nihil sunt molestias hic magnam sequi, reprehenderit quis maxime impedit, eos fugiat dolor commodi repellat. Atque voluptates rem dignissimos.
+        Impedit cum qui quas praesentium eius earum magni mollitia harum perferendis nobis voluptatem quasi quae voluptas beatae tenetur optio suscipit iure, ex atque dolor, facere eum. Reprehenderit, nemo vero. Sint.</p>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, optio neque? Nihil sunt molestias hic magnam sequi, reprehenderit quis maxime impedit, eos fugiat dolor commodi repellat. Atque voluptates rem dignissimos.
+        Impedit cum qui quas praesentium eius earum magni mollitia harum perferendis nobis voluptatem quasi quae voluptas beatae tenetur optio suscipit iure, ex atque dolor, facere eum. Reprehenderit, nemo vero. Sint.</p>
+
+    </div>
+  )
+}
